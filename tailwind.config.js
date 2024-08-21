551ae7fea3f291bcb99a03aa44a6bedf701e881b
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-08-21 10:56:45 +0300

    add: new colors to index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "light-gray-color": "var(--gray-light-color)",
+            "medium-gray-color": "var(--medium-gray-color)",
+            "pink-pastel-color": "var(--pink-pastel-color)",
+            "peach-color": "var(--peach-color)",
+            "ligth-green-color": "var(--light-green-color)",
+            "berry-lilac-color": "var(--berry-lilac-color)"
+        }
+    },
   },
   plugins: [],
 }
